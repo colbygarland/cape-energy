933f0102ed117fb5ac3701626c73206345012df8
Colby Garland <colbygarland@gmail.com>
2024-06-10 20:31:04 -0600

    added menu

---
+++ components/MenuItem.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps(['to', 'text', 'callback'])
+</script>
+
+<template>
+  <li class="mb-6 font-headings text-4xl text-white lg:text-lg">
+    <NuxtLink :to="to" class="transition duration-100 hover:text-gray-100" @click="callback">{{ text }}</NuxtLink>
+  </li>
+</template>
