310876340cebd7f2d5638c28440257c9aa5be023
Colby Garland <colbygarland@gmail.com>
2024-09-11 21:47:23 -0600

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="hero">
     <div class="relative py-32 lg:pt-96">
-      <div class="container relative z-50 text-white">
+      <div class="container relative z-50 text-white text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">
         <h1 class="mb-20 text-4xl font-normal leading-snug text-white lg:text-6xl">
           Cape Energy Group <br />Innovative Energy Solutions. <br />Leading the way to a safer Future.
         </h1>
